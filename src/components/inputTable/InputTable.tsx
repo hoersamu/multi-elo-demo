9223d9a8cd9f439fa4987820737ed279e01d965a
hoersamu <samuel@hoera.dev>
2022-03-30 08:48:56 +0200

    ♻️ moved tdc to inputtable file

--- src/components/inputTable/InputTable.tsx
+++ src/components/inputTable/InputTable.tsx
@@ -3,7 +3,6 @@ import classNames from "classnames";
 import { ChangeEvent, FC, InputHTMLAttributes } from "react";
 import { Team } from "../../App";
 import { Button } from "../button/Button";
-import { TableDataCell } from "../tableDataCell/TableDataCell";
 
 type InputTableProps = {
   teams: Team[];
@@ -94,4 +93,8 @@ const Input: FC<InputHTMLAttributes<HTMLInputElement>> = ({
     className={classNames("bg-e-2 h-8 rounded-lg px-2", className)}
     {...props}
   />
+);
+
+const TableDataCell: FC = ({ children }) => (
+  <td className="px-2">{children}</td>
 );

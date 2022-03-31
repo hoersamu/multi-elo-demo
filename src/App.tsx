8eb61bbb26e6ce7f85b74e7a1085b61da614717c
hoersamu <samuel@hoera.dev>
2022-03-31 08:54:13 +0200

    ♻️ extracted header content

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import React, { FC, useRef, useState } from "react";
 import { useEffectOnce } from "usehooks-ts";
 import { Button } from "./components/button/Button";
 import { ExtendedSettings } from "./components/extendedSettings/ExtendedSettings";
+import { Header } from "./components/header/Header";
 import { InputTable } from "./components/inputTable/InputTable";
 import { OutputTable } from "./components/outputTable/OutputTable";
 
@@ -76,25 +77,7 @@ export const App: FC = () => {
 
   return (
     <div className="bg-bg min-h-screen flex flex-col items-center">
-      <h1 className="text-4xl font-semibold mt-20 mb-4">Multi-Elo Demo</h1>
-      <p className="text-center">
-        {"This website showcases the functionality offered by my "}
-        <a
-          href="https://www.npmjs.com/package/multi-elo"
-          className="text-accent"
-        >
-          multi-elo npm package.
-        </a>
-      </p>
-      <p className="mb-8 text-center">
-        {"You can learn more about the Elo rating system "}
-        <a
-          href="https://en.wikipedia.org/wiki/Elo_rating_system"
-          className="text-accent"
-        >
-          here.
-        </a>
-      </p>
+      <Header />
       <InputTable
         teams={teams}
         setTeams={setTeams}
